037f8177b331e80ddfcd2adcd154bdd05d7f2fb5
Henrique Melo <hacmelo@gmail.com>
2022-11-19 12:41:52 -0300

    Refatoracao na logica add matriz do tabuleiro para referencia alteracao na geracao das bombas e numero

--- script.js
+++ script.js
@@ -1,121 +1,213 @@
 const difficulties = [
-    { id: 'facil', boardWidth: 6, boardHeight: 5, bombs: 10 },
-    { id: 'media', boardWidth: 10, boardHeight: 9, bombs: 20 },
+    { id: 'facil', boardWidth: 6, boardHeight: 5, bombs: 8 },
+    { id: 'media', boardWidth: 10, boardHeight: 9, bombs: 17 },
     { id: 'dificil', boardWidth: 15, boardHeight: 13, bombs: 40 },
 ];
 
-let bombs = [];
-
-let timer;
+let board = [];
 
 let isGameRunning = false;
+
+let timer;
 
 window.onload = () => {
     window.addEventListener("contextmenu", e => e.preventDefault());
     startGame();
+    listenToClickEvents();
 }
 
-function startGame() {
+function listenToClickEvents() {
     listenToDifficulty();
-    checkDifficulty();
     listenToRestart();
-}
-
-function checkDifficulty() {
-    const savedDifficulty = window.localStorage.getItem("difficulty");
-    const selectedDifficulty = savedDifficulty ?? 'facil';
-    difficulties.forEach(difficulty => {
-        const element = document.getElementById(difficulty.id);
-        const bombsInfo = document.getElementById("bombs-left");
-        if (difficulty.id === selectedDifficulty) {
-            bombsInfo.innerText = difficulty.bombs
-            element.classList.add('option-active');
-            drawBoard(difficulty);
-        }
-        else element.classList.remove('option-active');
-    })
 }
 
 function listenToDifficulty() {
     difficulties.forEach(difficulty => {
-        const element = document.getElementById(difficulty.id)
-        element.addEventListener("click", () => {
-            if (element.classList.contains("option-active")) return;
+        const element$ = $(`#${difficulty.id}`)
+        element$.click(() => {
+            if (element$.hasClass("option-active")) return;
             const response = window.confirm("Alterar a dificuldade reinicia o jogo. Deseja prosseguir?")
             if (response) {
                 window.localStorage.setItem("difficulty", difficulty.id);
-                checkDifficulty();
+                startGame();
             }
         });
     });
 }
 
 function listenToRestart() {
-    const element = document.getElementById("restart")
-    element.addEventListener("click", () => {
+    const element$ = $("#restart");
+    element$.click(() => {
         const response = window.confirm("Todo seu progresso será perdido. Deseja reiniciar?")
-        if (response) checkDifficulty();
+        if (response) startGame()
     });
 }
 
-function drawBoard(difficulty) {
+function startGame() {
     clearTimer();
+    const difficulty = loadDifficulty();
+    generateBoard(difficulty);
+    drawBoard(difficulty);
+}
+
+function loadDifficulty() {
+    const targetDifficulty = window.localStorage.getItem("difficulty");
+    let difficulty = difficulties[0];
+
+    difficulties.forEach(item => {
+        const element$ = $(`#${item.id}`);
+
+        if (item.id === targetDifficulty) {
+            difficulty = item;
+            element$.addClass("option-active");
+
+            const bombsLeft$ = $("#bombs-left");
+            bombsLeft$.text(difficulty.bombs);
+        } else {
+            element$.removeClass("option-active");
+        }
+    });
+
+    return difficulty;
+}
+
+function generateBoard({ boardWidth, boardHeight, bombs }) {
+    board = [];
+
+    for (x = 0; x < boardHeight; x++) {
+        if (!board[x]) board[x] = []
+
+        for (y = 0; y < boardWidth; y++) {
+            if (x % 2 == 0 && y == boardWidth - 1) {
+                board[x][y] = null;
+            } else {
+                board[x][y] = {
+                    value: 0,
+                    isFlagged: false,
+                    isSelected: false,
+                };
+            }
+        }
+    }
+
+    insertBombs(boardWidth, boardHeight, bombs);
+    updateNumbers(boardWidth, boardHeight);
+}
+
+function insertBombs(boardWidth, boardHeight, bombs) {
+    for (let i = 0; i < bombs; i++) {
+        const x = Math.floor(Math.random() * boardHeight);
+        const y = Math.floor(Math.random() * boardWidth);
+
+        if (board[x][y] == null || board[x][y].value == "💣")  i--;
+        else board[x][y].value = "💣";
+    }
+}
+
+function updateNumbers(boardWidth, boardHeight) {
+    for (x = 0; x < boardHeight; x++) {
+        for (y = 0; y < boardWidth; y++) {
+            if (board[x][y]?.value == "💣") {
+                const modifier = x % 2 != 0 ? -1 : 0
+                setNumber(x, y - 1);
+                setNumber(x, y + 1);
+                setNumber(x - 1, y + modifier);
+                setNumber(x - 1, y + 1 + modifier);
+                setNumber(x + 1, y + modifier);
+                setNumber(x + 1, y + 1 + modifier);
+            }
+        }
+    }
+}
+
+function setNumber(x, y) {
+    if (
+        x >= board.length || y >= board[0].length ||
+        x < 0 || y < 0 ||
+        board[x][y] == null || board[x][y].value == "💣"
+    ) return;
+    board[x][y].value += 1;
+}
+
+function drawBoard({ boardWidth, boardHeight }) {
     const container = document.getElementById("board");
     container.replaceChildren();
-    
-    for(let i = 0; i < difficulty.boardHeight; i++){
+
+    for (x = 0; x < boardHeight; x++) {
         const $hexRow = $("<div></div>").addClass("hex-row")
-        let cont = difficulty.boardWidth;
-        if(i%2 == 0) {
-            $hexRow.addClass("even");
-            cont--;
-        }
-        
-        for(let j = 0; j < cont; j++) {
-            _drawHexBoard($hexRow, j, i);
+        if(x % 2 == 0) $hexRow.addClass("even");
+
+        for (y = 0; y < boardWidth; y++) {
+            if (board[x][y] == null) continue;
+            const $hex = $("<div></div>").attr("id", `${x}-${y}`);
+            $hex.addClass("hex");
+            $hex.mousedown((event) => { mouseClickEvent(event) });
+
+            const $hexTop = $("<div></div>").addClass("top");
+            const $hexMiddle = $("<div></div>").addClass("middle");
+            // TODO remover
+            $hexMiddle.text(board[x][y].value)
+            const $hexBottom = $("<div></div>").addClass("bottom");
+
+            $hex.append($hexTop, $hexMiddle, $hexBottom);
+            $hexRow.append($hex);
+            $("#board").append($hexRow);
         }
     }
-
-    bombs = [];
-    generateBombs(difficulty);
-}
-
-function generateBombs(difficulty) {
-    for(let i = 0; i < difficulty.bombs; i++) {
-        const y = Math.floor(Math.random() * difficulty.boardHeight);
-        const x = Math.floor(Math.random() * (difficulty.boardWidth + (y % 2 == 0 ? -1 : 0)));
-    
-        const $idTile = $(`div.hex#${y}-${x} .middle`);
-    
-        if($idTile.text() == "") $idTile.text("💣");
-    }
-}
-
-function _drawHexBoard($hexRow, col, row) {
-    const $hex = $("<div></div>").attr("id", `${row}-${col}`);
-    $hex.addClass("hex");
-    $hex.mousedown((event) => { mouseClickEvent(event) });
-
-    const $hexTop = $("<div></div>").addClass("top");
-    const $hexMiddle = $("<div></div>").addClass("middle");
-    const $hexBottom = $("<div></div>").addClass("bottom");
-
-    $hexRow.append($hex);
-    $hex.append($hexTop, $hexMiddle, $hexBottom);
-    $("#board").append($hexRow);
 }
 
 function mouseClickEvent(event) {
     if (isGameRunning === false) startTimer();
+
     const $hexTile = $(event.currentTarget);
     switch (event.which) {
         case 1:
-            selectHexTile($hexTile);
+            handleSelection($hexTile);
             break;
         case 3:
-            toogleFlaggedHexTile($hexTile);
+            toggleFlag($hexTile);
             break;
     }
+}
+
+function toggleFlag($hexTile) {
+    const isSelected = $hexTile.children().hasClass("selected");
+    if (isSelected) return;
+
+    const hasFlag = $hexTile.find(".middle").text() == "🚩";
+    const { x, y } = getTileCoord($hexTile.attr("id"));
+    board[x][y].isFlagged = !hasFlag;
+
+    if (hasFlag) {
+        $hexTile.find(".middle").text("");
+        $hexTile.children().removeClass("flagged");
+    } else {
+        $hexTile.find(".middle").text("🚩");
+        $hexTile.children().addClass("flagged");
+    }
+}
+
+function handleSelection($hexTile) {
+    const hasFlag = $hexTile.find(".middle").text() == "🚩";
+    if (hasFlag) return;
+
+    const { x, y } = getTileCoord($hexTile.attr("id"));
+    board[x][y].isSelected = true;
+
+    $hexTile.children().addClass("selected");
+    $hexTile.find(".middle").text(board[x][y].value);
+
+    if (board[x][y].value == '💣') gameOver();
+}
+
+function gameOver() {
+}
+
+function getTileCoord(id) {
+    const coordArray = id.split("-");
+    const x = coordArray[0];
+    const y = coordArray[1];
+    return { x, y }
 }
 
 function startTimer() {
@@ -133,56 +225,4 @@ function clearTimer() {
     isGameRunning = false;
     clearInterval(timer);
     document.getElementById("timer").innerText = "0 (s)";
-}
-
-function selectHexTile($hexTile) {
-    const hasNoFlag = !($hexTile.find(".middle").text() == "🚩");
-    if(hasNoFlag){
-        $hexTile.children().addClass("selected");
-
-        const hasNoBomb = !($hexTile.find(".middle").text() == "💣");
-        if(hasNoBomb) {
-            $hexTile.find(".middle").text(getBombsNumber($hexTile));
-        }
-    }
-}
-
-function getBombsNumber($hexTile) {
-    let cont = 0;
-    getNeighborsHexTile($hexTile).forEach((tile) => {
-        tile[0].innerText == "💣" && cont++;
-    });
-    return cont;
-}
-
-function getNeighborsHexTile($hexTile) {
-    const y = parseInt($hexTile[0].id.split("-")[0]);
-    const x = parseInt($hexTile[0].id.split("-")[1]);
-
-    let neighbors = [];
-    neighbors.push($(`#${y}-${x-1}`));
-    neighbors.push($(`#${y}-${x+1}`));
-    neighbors.push($(`#${y-1}-${x}`));
-    neighbors.push($(`#${y+1}-${x}`));
-    const diagonal = y % 2 === 0 ? 1 : -1;
-    neighbors.push($(`#${y-1}-${x+diagonal}`));
-    neighbors.push($(`#${y+1}-${x+diagonal}`));
-
-    let result = neighbors.filter(function (element) {
-        return element[0] != undefined;
-    });
-
-    return result;
-}
-
-function toogleFlaggedHexTile($hexTile) {
-    const hasFlag = !!($hexTile.find(".middle").text() == "🚩");
-    const isNotSelected = !($hexTile.children().hasClass("selected"));
-    if(hasFlag){
-        $hexTile.find(".middle").text("");
-        $hexTile.children().removeClass("flagged");
-    }else if(isNotSelected){
-        $hexTile.find(".middle").text("🚩");
-        $hexTile.children().addClass("flagged");
-    }
 }
